0af1bc3006f13be658e3948a35e4046a8a1afdba
YM CHOI <escriboy@gmail.com>
2022-12-13 13:57:50 +0900

    youtube clone coding

---
+++ src/components/ChannelInfo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
+
+export default function ChannelInfo({ id, name }) {
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: url,
+  } = useQuery(["channel", id], () => youtube.channelImageURL(id), {
+    staleTime: 1000 * 60 * 5,
+  });
+  // url을 가져오지 못하면 채널의 앞 글자를 딴 동그란 아이콘으로 대체하는 것 해 보기
+
+  return (
+    <div className="flex my-4 mb-8 items-center">
+      {url && <img className="w-10 h-10 rounded-full" src={url} alt={name} />}
+      <p className="text-lg font-medium ml-2">{name}</p>
+    </div>
+  );
+}
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { formatAgo } from "../util/date";
+import { useNavigate } from "react-router-dom";
+
+export default function VideoCard({ video, type }) {
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
+  const isList = type === "list";
+  return (
+    <li
+      className={isList ? "flex gap-1 m-2" : ""}
+      onClick={() => {
+        // 리액트 라우터에서 부가적인 객체들을 전달할 때 두 번째 인자에 객체를 전달해주면 된다.
+        navigate(`/videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
+      <img
+        className={isList ? "w-60 mr-2" : "w-full"}
+        src={thumbnails.medium.url}
+        alt={title}
+      />
+      <div>
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{formatAgo(publishedAt)}</p>
+      </div>
+    </li>
+  );
+}
